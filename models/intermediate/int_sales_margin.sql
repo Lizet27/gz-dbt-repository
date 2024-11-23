27410baa082d4848f0fe53c172a557bbae96c7eb
Lizet Ortiz Rojas <ldiana27.ldor@gmail.com>
2024-11-23 21:11:08 +0000

    add  puchase cost and margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,7 @@
+select 
+*,
+round(quantity * purchase_price,2) as purchase_cost ,
+round(revenue - (quantity * purchase_price),2) as margin 
+FROM {{ref("stg_raw__sales")}}
+JOIN {{ref("stg_raw__product")}}
+USING (products_id)
--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -12,7 +12,7 @@ renamed as (
         orders_id,
         shipping_fee,
         logcost,
-        cast(ship_cost as integer) as ship_cost
+        cast(ship_cost as int64) as ship_cost
 
     from source
 
